410bc264f3f1f63ffe251c1a8effeeee973a3d8b
Vinicius <vinimateos.s@gmail.com>
2023-02-28 10:24:04 -0300

    bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pw3-angular';
+
+
+  obterAutor(){
+    return "Rogério de Morais";
+  }
+
+  nome = 'Anderson';
+  sobrenome = ' Silva';
+  idade = '57';
+  logo = "favicon.ico";
+
+  nomeCompleto(){
+    return this.nome + this.sobrenome;
+  }
+
+
 }
